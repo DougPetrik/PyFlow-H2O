2bbfde41bd4a3b268de298d97d6f7ce448c417af
douga <Pt%VmD#qG0ErmO5S>
2021-04-15 20:14:42 -0500

    feat: read config file

--- src/pyflow_h2o/main.py
+++ src/pyflow_h2o/main.py
@@ -2,3 +2,17 @@ import tkinter as tk
 
 import configparser
 import os
+
+# this script's file path
+app_dir = os.path.dirname(os.path.abspath(__file__))
+
+# initialize config reader
+config_parser = configparser.RawConfigParser()
+config_file_path = os.path.join(app_dir, 'config.ini')
+config_parser.read(config_file_path)
+
+def read_config(parser_instance, setting, attribute):
+    return config_parser.get(setting, attribute)
+
+
+
