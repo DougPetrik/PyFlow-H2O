ca44ed54937b1bf1d0dce56a1597ebae88a63b90
dpetrik <Pt%VmD#qG0ErmO5S>
2021-04-16 11:34:10 -0500

    feat: add main canvas

--- src/pyflow_h2o/main.py
+++ src/pyflow_h2o/main.py
@@ -14,11 +14,21 @@ config_parser.read(config_file_path)
 def read_config(parser_instance, setting, attribute):
     return config_parser.get(setting, attribute)
 
+class Main(tk.Frame):
+    def __init__(self, parent):
+        self.parent = parent
+        self.width = read_config(config_parser, 'RESOLUTION', 'width')
+        self.height = read_config(config_parser, 'RESOLUTION', 'height')
+        self.canvas = tk.Canvas(parent.frame, width=self.width, height=self.height)
+
 
 class MainApplication(tk.Frame):
     def __init__(self, parent,*args, **kwargs):
         self.parent = parent
         self.frame = tk.Frame.__init__(self, parent, *args, **kwargs)
+
+        self.main  = Main(self)
+        self.main.canvas.pack()
 
 
 if __name__ == '__main__':
